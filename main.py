01a48ea49ff36d29889177d1cdccc9150151a9eb
José Luis Flores García <33033155+house117@users.noreply.github.com>
2018-09-02 00:00:46 -0500

    Adding Layouts and Buttons (working)

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
 import cv2
 
 
@@ -19,21 +22,29 @@ class KivyCamera(Image):
             buf1 = cv2.flip(frame, 0)
             buf = buf1.tostring()
             image_texture = Texture.create(
-                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+                size=(frame.shape[1], frame.shape[0]), colorfmt='rgba')
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
 
-
-class CamApp(App):
+class MainFrame(BoxLayout):
     def build(self):
+        btn1 = Button(text='hello', size=(200,100))
+        self.add_widget(btn1)
         self.capture = cv2.VideoCapture(0)
-        self.my_camera = KivyCamera(capture=self.capture, fps=30)
-        return self.my_camera
+        #self.my_camera = KivyCamera(capture=self.capture, fps=60)
+        self.add_widget(KivyCamera(capture=self.capture, fps=60))
+        return self
 
     def on_stop(self):
         #without this, app will not exit even if the window is closed
         self.capture.release()
+        
+
+class CamApp(App):
+    def build(self):
+        frame = MainFrame()
+        return frame.build()
 
 
 if __name__ == '__main__':

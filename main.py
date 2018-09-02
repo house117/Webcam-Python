3580f44612c37ecfe19c7baa046ee8357511b551
José Luis Flores García <33033155+house117@users.noreply.github.com>
2018-09-02 01:41:49 -0500

    Picture button working, next step is Frame design, and some options

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-
+import time
 import cv2
-
+from functools import partial
 
 class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
@@ -26,12 +26,31 @@ class KivyCamera(Image):
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
-
+#Lo que hace el botonciiito
+def callback(self, event, param):
+        cap = cv2.VideoCapture(0)
+        ret,frame = cap.read()
+        '''
+        while(True):
+            cv2.imshow('img1',frame) #display the captured image
+            if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
+                cv2.imwrite('c1.png',frame)
+                cv2.destroyAllWindows()
+                break
+                '''
+        cv2.imwrite('c1.png', frame);
+        print('Se tomo foto prro')
+        cap.release()
+            
 class MainFrame(BoxLayout):
+    
     def build(self):
-        btn1 = Button(text='hello', size=(200,100))
-        self.add_widget(btn1)
+        
         self.capture = cv2.VideoCapture(0)
+        btn1 = Button(text='FOTITO PERRO', size=(200,100))
+        arg = 123
+        btn1.bind(on_press=self.auth)
+        self.add_widget(btn1)
         #self.my_camera = KivyCamera(capture=self.capture, fps=60)
         self.add_widget(KivyCamera(capture=self.capture, fps=60))
         return self
@@ -39,13 +58,18 @@ class MainFrame(BoxLayout):
     def on_stop(self):
         #without this, app will not exit even if the window is closed
         self.capture.release()
+    def auth(self, instance):
+        x = 0
+        print('auth called')
+        ret,foto = self.capture.read()
+        cv2.imwrite('capture'+str(x)+'.png', foto)
+        x=x+1
+        print('El valor de x esta en: '+str(x))
         
-
+    
 class CamApp(App):
     def build(self):
         frame = MainFrame()
         return frame.build()
-
-
 if __name__ == '__main__':
     CamApp().run()

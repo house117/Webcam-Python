2e1e1cb3e3120994281ce7a9948bb5ebad7af740
José Luis Flores García <33033155+house117@users.noreply.github.com>
2018-09-02 22:56:05 -0500

    finished v1.0, all features working

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+from PIL import Image as imaag
 import time
 import cv2
+import os
 from functools import partial
 
-Config.set('graphics', 'width', '625')
-Config.set('graphics', 'height', '650')
+Config.set('graphics', 'width', '700')
+Config.set('graphics', 'height', '710')
 Config.set('graphics','resizable',0)
 class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
@@ -35,21 +37,6 @@ class KivyCamera(Image):
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
-#Lo que hace el botonciiito (por ahora no usamos esto)
-def callback(self, event, param):
-        cap = cv2.VideoCapture(0)
-        ret,frame = cap.read()
-        '''
-        while(True):
-            cv2.imshow('img1',frame) #display the captured image
-            if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
-                cv2.imwrite('c1.png',frame)
-                cv2.destroyAllWindows()
-                break
-                '''
-        cv2.imwrite('c1.png', frame);
-        print('Se tomo foto prro')
-        cap.release()
             
 class MainFrame(GridLayout):
     
@@ -60,6 +47,10 @@ class MainFrame(GridLayout):
         self.orientation = 'vertical'
         self.capture = cv2.VideoCapture(0)
         #self.my_camera = KivyCamera(capture=self.capture, fps=60)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,1280);
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,720);
+        self.camera = KivyCamera(capture=self.capture, fps=60)
+        
         self.add_widget(KivyCamera(capture=self.capture, fps=60))
         #Down Part of the frame
         LayoutBotonCaptura = FloatLayout(size_hint_x=1, size_hint_y =.4)
@@ -85,32 +76,47 @@ class MainFrame(GridLayout):
                                        pos=(310, 60))
         LayoutBotonesFormato.orientation = 'vertical'
         lblTitleFormato = Label(text='Selección de formato:')
-        btnColorPNG = ToggleButton(text='PNG', group='Formato', state='down')
-        btnColorJPG = ToggleButton(text='JPG', group='Formato')
-        btnColorGIF = ToggleButton(text='GIF', group='Formato')
-        btnColorBMP = ToggleButton(text='BMP', group='Formato')
+        btnColorPNG = ToggleButton(text='PNG', group='format', state='down')
+        btnColorJPG = ToggleButton(text='JPG', group='format')
+        btnColorGIF = ToggleButton(text='GIF', group='format')
+        btnColorBMP = ToggleButton(text='BMP', group='format')
         LayoutBotonesFormato.add_widget(lblTitleFormato)
         LayoutBotonesFormato.add_widget(btnColorPNG)
         LayoutBotonesFormato.add_widget(btnColorJPG)
         LayoutBotonesFormato.add_widget(btnColorGIF)
         LayoutBotonesFormato.add_widget(btnColorBMP)
-
+        #Group of buttons to select the size
+        LayoutBotonesRes = BoxLayout(size_hint=(.2, .8),
+                                       pos=(500, 60))
+        LayoutBotonesRes.orientation = 'vertical'
+        lblTitleRes = Label(text='Resolucion:')
+        btnColor720 = ToggleButton(text='1280x720', group='size', state='down')
+        btnColor480 = ToggleButton(text='800x600', group='size')
+        btnColor360 = ToggleButton(text='640x360', group='size')
+        LayoutBotonesRes.add_widget(lblTitleRes)
+        LayoutBotonesRes.add_widget(btnColor720)
+        LayoutBotonesRes.add_widget(btnColor480)
+        LayoutBotonesRes.add_widget(btnColor360)
         #Adding Layouts to lower layout
         LayoutBotonCaptura.add_widget(btnCaptura)
         LayoutBotonCaptura.add_widget(LayoutBotonesColor)
         LayoutBotonCaptura.add_widget(LayoutBotonesFormato)
+        LayoutBotonCaptura.add_widget(LayoutBotonesRes)
         self.add_widget(LayoutBotonCaptura)
         return self
 
     def on_stop(self):
         #without this, app will not exit even if the window is closed
         self.capture.release()
+
     def auth(self, instance):
         #Obtenemos fecha para el nombre de la imagen
         timestr = time.strftime("%Y%m%d-%H%M%S")
         #Obtenemos el color
         currentColor=[t for t in ToggleButton.get_widgets('color') if t.state=='down'][0]
-        currentFormat=[t for t in ToggleButton.get_widgets('color') if t.state=='down'][0]
+        currentFormat=[f for f in ToggleButton.get_widgets('format') if f.state=='down'][0]
+        currentSize=[s for s in ToggleButton.get_widgets('size') if s.state=='down'][0]
+
         print(currentColor.text)
         ret,foto = self.capture.read()
         if currentColor.text == 'A color':
@@ -129,18 +135,105 @@ class MainFrame(GridLayout):
             foto = cv2.adaptiveThreshold(foto,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY, 11, 2)
 
-        #FORMATO A GUARDAR
-        if currentColor.text == 'PNG':
+        #Format to save.
+        if currentFormat.text == 'PNG':
             print('Pidio PNG el prro')
-        elif currentColor.text == 'JPG':
-            print('Pidio JPG el prro')
-        elif currentColor.text == 'GIF':
-            print('La pidio GIF el prro')
-        elif currentColor.text == 'BMP':
+            compression_params = [cv2.IMWRITE_PNG_COMPRESSION, 9] 
+            cv2.imwrite(timestr+'.png', foto, compression_params)
+            if currentSize.text == '800x600':
+                    print('Entro 360')
+                    width = 800
+                    height = 600
+                    im = imaag.open(timestr+'.png')
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('800600'+timestr+'.png')
+            elif currentSize.text == '640x360':
+                print('Entro 360')
+                width = 640
+                height = 360
+                im = imaag.open(timestr+'.gif')
+                im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                im2.save('640360'+timestr+'.gif')
+        elif currentFormat.text == 'JPG':
+            cv2.imwrite(timestr+'.jpg', foto, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+            if currentSize.text == '800x600':
+                    print('Entro 360')
+                    width = 800
+                    height = 600
+                    im = imaag.open(timestr+'.jpg')
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('800600'+timestr+'.jpg')
+            elif currentSize.text == '640x360':
+                print('Entro 360')
+                width = 640
+                height = 360
+                im = imaag.open(timestr+'.gif')
+                im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                im2.save('640360'+timestr+'.gif')
+        elif currentFormat.text == 'GIF':
+            compression_params = [cv2.IMWRITE_PNG_COMPRESSION, 9] 
+            cv2.imwrite(timestr+'.png', foto, compression_params)
+            im = imaag.open(timestr+".png")
+            im = im.convert('RGB').convert('P', palette=imaag.ADAPTIVE)
+            os.remove(timestr+".png")
+            im.save(timestr+'.gif')
+            if currentSize.text == '800x600':
+                    print('Entro 360')
+                    width = 800
+                    height = 600
+                    im = imaag.open(timestr+'.gif')
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('800600'+timestr+'.gif')
+            elif currentSize.text == '640x360':
+                print('Entro 360')
+                width = 640
+                height = 360
+                im = imaag.open(timestr+'.gif')
+                im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                im2.save('640360'+timestr+'.gif')
+        elif currentFormat.text == 'BMP':
             print('La pidio BMP el prro')
-        
-        cv2.imwrite(timestr+'.png', foto)
-        
+            compression_params = [cv2.IMWRITE_PNG_COMPRESSION, 9] 
+            cv2.imwrite(timestr+'.png', foto, compression_params)
+            img = imaag.open(timestr+".png")
+            file_out = timestr+'.bmp'
+            if len(img.split()) == 4:
+                # prevent IOError: cannot write mode RGBA as BMP
+                r, g, b, a = img.split()
+                img = imaag.merge("RGB", (r, g, b))
+                if currentSize.text == '800x600':
+                    print('Entro 360')
+                    width = 800
+                    height = 600
+                    im = imaag.open(file_out)
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('800600'+file_out)
+                elif currentSize.text == '640x360':
+                    print('Entro 360')
+                    width = 640
+                    height = 360
+                    im = imaag.open(file_out)
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('640360'+file_out)
+            else:
+                img.save(file_out)
+                if currentSize.text == '800x600':
+                    width = 800
+                    height = 600
+                    im = imaag.open(file_out)
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('800600'+file_out)
+                elif currentSize.text == '640x360':
+                    print('Entro 360')
+                    width = 640
+                    height = 360
+                    im = imaag.open(file_out)
+                    im2 = im.resize((width, height), imaag.ANTIALIAS) 
+                    im2.save('640360'+file_out)
+            os.remove(timestr+".png")
+        else:
+            cv2.imwrite(timestr+'.png', foto)
+        #Resize if necesary
     
 class CamApp(App):
     def build(self):
